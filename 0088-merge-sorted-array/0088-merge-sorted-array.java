a2893bcd1eb3ffd437f96438a37d828c222a69a7
Jaiprasath Madheswaran <jaiprasath.work@gmail.com>
2026-09-16 14:28:30 +0530

    Time: 4 ms (16.18%), Space: 44.1 MB (16.47%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,7 +1,5 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int arrLen = m + n;
-        System.out.println("m :: " + m + " n :: " + n );
         for(int i = 0; i < n; i++ ){
             nums1[i+m] = nums2[i];
         }
